66014b3f9fd684b7b099a64bda8140bdeeeaca68
vanshpanwar191 <vpanwar_be24@thapar.edu>
2025-08-30 13:55:24 +0530

    Create a2_2.cpp

---
+++ a2_2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class temp{
+  int t;
+  public:
+    void input(){
+      cout<<"enter the temperature in celsius:/n";
+      cin>>t;
+    }
+    
+    void calcd(){
+      int F=((9*t)/5)+32;
+      cout<<F;
+    }
+};
+int main(){
+  temp ob;
+  ob.input();
+  ob.calcd();
+  
+  
+}
