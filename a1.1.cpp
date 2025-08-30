d93f2d8a76c2bd98ca3fa2fa050dc39c4438e608
vanshpanwar191 <vpanwar_be24@thapar.edu>
2025-08-30 13:46:01 +0530

    Create a1.1.cpp

---
+++ a1.1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class A{
+  void display(){
+    cout<<"hello world/n"<<"/t"<<"hey /t how you are doing ";
+  }
+};
+int main{
+  A ob;
+  ob.display();
+  return 0;
+}
