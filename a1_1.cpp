83963c3f9722a9618310054fd57ef27de73d8fc3
vanshpanwar191 <vpanwar_be24@thapar.edu>
2025-08-30 14:01:43 +0530

    Update a1_1.cpp

--- a1_1.cpp
+++ a1_1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 class A{
   void display(){
-    cout<<"hello world/n"<<"/t"<<"hey /t how you are doing ";
+    cout<<"hello world\n"<<"\t"<<"hey \t how you are doing ";
   }
 };
 int main{
