95ebecbb6fcab1c8f8f8bb159fdca7d75f5ecc40
vanshpanwar191 <vpanwar_be24@thapar.edu>
2025-08-30 16:36:20 +0530

    Update a1_5.cpp

--- a1_5.cpp
+++ a1_5.cpp
@@ -1,4 +1,4 @@
-#incldue<iostream>
+#include<iostream>
 using namespace std;
 class student {
   string name;
@@ -6,5 +6,34 @@ class student {
   string deg;
   string hostel;
   int cg;
+  public:
+    void adddetails(student *n,int size){
+      for(int i=0;i<size;i++){
+          cout<<"enter the name:\n";
+          cin.ignore();
+          getline(cin,n[i].name);
+          cout<<"enter the roll number:\n";
+          cin>>n[i].rn;
+          cout<<"enter the degree:\n";
+          cin.ignore();
+          getline(cin,n[i].degree);
+          cout<<"enter the hostel:\n";
+          cin.ignore();
+          getline(cin,n[i].hostel);
+          cout<<"enter the cg:\n";
+          cin>>n[i].cg;
+          
+        }
+         
+      
+    }
   
 };
+int main(){
+  int size;
+  cout<<"enter the number of students:\n";
+  cin>>size;
+  student s[n];
+  
+}
+
