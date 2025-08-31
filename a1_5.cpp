80c03c8cb26971c3b6c5741f5880f8f049e76802
vanshpanwar191 <vpanwar_be24@thapar.edu>
2025-08-31 17:45:50 +0530

    Update a1_5.cpp

--- a1_5.cpp
+++ a1_5.cpp
@@ -1,39 +1,137 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 class student {
-  string name;
-  int rn;
-  string deg;
-  string hostel;
-  int cg;
-  public:
-    void adddetails(student *n,int size){
-      for(int i=0;i<size;i++){
-          cout<<"enter the name:\n";
-          cin.ignore();
-          getline(cin,n[i].name);
-          cout<<"enter the roll number:\n";
-          cin>>n[i].rn;
-          cout<<"enter the degree:\n";
-          cin.ignore();
-          getline(cin,n[i].degree);
-          cout<<"enter the hostel:\n";
-          cin.ignore();
-          getline(cin,n[i].hostel);
-          cout<<"enter the cg:\n";
-          cin>>n[i].cg;
-          
+    string name;
+    int rn;
+    string deg;
+    string hostel;
+    int cg;
+
+public:
+    // Input details for all students
+    static void adddetails(student *n, int size) {
+        for (int i = 0; i < size; i++) {
+            cout << "Enter the name: ";
+            cin.ignore();
+            getline(cin, n[i].name);
+            cout << "Enter the roll number: ";
+            cin >> n[i].rn;
+            cin.ignore();
+            cout << "Enter the degree: ";
+            getline(cin, n[i].deg);
+            cout << "Enter the hostel: ";
+            getline(cin, n[i].hostel);
+            cout << "Enter the CGPA: ";
+            cin >> n[i].cg;
         }
-         
-      
     }
-  
+
+    // Update full details based on student name
+    static void updatedetails(student *n, int size) {
+        string targetName;
+        cout << "Enter the name of the student to update: ";
+        cin.ignore();
+        getline(cin, targetName);
+
+        for (int i = 0; i < size; i++) {
+            if (n[i].name == targetName) {
+                cout << "Enter new name: ";
+                getline(cin, n[i].name);
+                cout << "Enter new roll number: ";
+                cin >> n[i].rn;
+                cin.ignore();
+                cout << "Enter new degree: ";
+                getline(cin, n[i].deg);
+                cout << "Enter new hostel: ";
+                getline(cin, n[i].hostel);
+                cout << "Enter new CGPA: ";
+                cin >> n[i].cg;
+                break;
+            }
+        }
+    }
+
+    // Update CGPA
+    static void updatecgpa(student *n, int size) {
+        int roll;
+        cout << "Enter the roll number of the student: ";
+        cin >> roll;
+
+        for (int i = 0; i < size; i++) {
+            if (n[i].rn == roll) {
+                cout << "Enter new CGPA: ";
+                cin >> n[i].cg;
+                break;
+            }
+        }
+    }
+
+    // Update Hostel
+    static void updatehostel(student *n, int size) {
+        int roll;
+        cout << "Enter the roll number: ";
+        cin >> roll;
+
+        for (int i = 0; i < size; i++) {
+            if (n[i].rn == roll) {
+                cout << "Enter new hostel: ";
+                cin.ignore();
+                getline(cin, n[i].hostel);
+                break;
+            }
+        }
+    }
+
+    // Display all students
+    static void displaydetails(student *n, int size) {
+        for (int i = 0; i < size; i++) {
+            cout << "\nStudent " << i + 1 << ":\n";
+            cout << "Name: " << n[i].name << endl;
+            cout << "Roll No: " << n[i].rn << endl;
+            cout << "Degree: " << n[i].deg << endl;
+            cout << "Hostel: " << n[i].hostel << endl;
+            cout << "CGPA: " << n[i].cg << endl;
+        }
+    }
 };
-int main(){
-  int size;
-  cout<<"enter the number of students:\n";
-  cin>>size;
-  student s[n];
-  
+
+int main() {
+    int size;
+    cout << "Enter the number of students: ";
+    cin >> size;
+
+    student *s = new student[size]; // dynamic array
+
+    // Add details
+    student::adddetails(s, size);
+
+    char input;
+    cout << "Do you want to update details? (Y/N): ";
+    cin >> input;
+    if (input == 'Y') {
+        student::updatedetails(s, size);
+    }
+
+    cout << "Do you want to update CGPA? (Y/N): ";
+    cin >> input;
+    if (input == 'Y') {
+        student::updatecgpa(s, size);
+    }
+
+    cout << "Do you want to update hostel? (Y/N): ";
+    cin >> input;
+    if (input == 'Y') {
+        student::updatehostel(s, size);
+    }
+
+    // Final display
+    student::displaydetails(s, size);
+
+    delete[] s; // free memory
+    return 0;
 }
+
+  
+ 
 
